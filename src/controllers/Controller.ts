f6488f99eece40e05bfbde99562712f1efb389a2
Álvaro <62076470+FloatingHero@users.noreply.github.com>
2023-06-18 01:29:22 +0100

    updated npm packages

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 
 class Controller {
-  index(req: Request, res: Response) {
-    res.json({
+  index(req: Request, res: Response): Response {
+    return res.json({
       message:
         'Hello! This is a initial setup for your ts-express project. Feel free to use as you want and dont\'t forget give me a star on my GitHub profile if u like it! :)',
     });
